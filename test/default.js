bfd37e3345ea32bab90fac222220bb3844c921e6
em <michael@joinbox.com>
2014-12-09 18:30:23 +0100

    added tests, first stable version

--- test/default.js
+++ test/default.js
@@ -2,18 +2,139 @@
 	
 	var   Class 		= require('ee-class')
 		, log 			= require('ee-log')
-		, assert 		= require('assert')
-		, travis 		= require('ee-travis');
+		, assert 		= require('assert');
 
 
 
-	var Something = require('../')
+	var Message = require('../')
 
 
 
-	describe('Something', function(){
-		it('should', function(){
+	describe('The Message', function(){
+		it('should not throw erros when instantiated', function(){
+			new Message();
+		});
 
-		});		
+		it('should accept an uid sender via its constructor', function(){
+			new Message({
+				sender: 'uid---------d'
+			});
+		});	
+
+		it('should accept an object sender via its constructor', function(){
+			new Message({
+				sender: {
+				 	  id: 'broker'
+					, applicationId: 'tradr'
+					, version: '0.1.0'
+				}
+			});
+		});	
+
+		it('should accept an uid receipient via its constructor', function(){
+			new Message({
+				receipient: 'uid---------d'
+			});
+		});	
+
+		it('should accept an object receipient via its constructor', function(){
+			new Message({
+				receipient: {
+				 	  id: 'broker'
+					, applicationId: 'tradr'
+					, version: '0.1.0'
+				}
+			});
+		});	
+
+		
+
+		it('should return an uid sender set via its constructor', function(){
+			var message = new Message({
+				sender: 'uid---------d'
+			});
+
+			assert.equal(JSON.stringify(message.sender), '{"uid":"uid---------d"}');
+		});	
+
+		it('should return an object sender set via its constructor', function(){
+			var message = new Message({
+				sender: {
+				 	  id: 'broker'
+					, applicationId: 'tradr'
+					, version: '0.1.0'
+				}
+			});
+
+			assert.equal(JSON.stringify(message.sender), '{"id":"broker","applicationId":"tradr","version":"0.1.0"}');
+		});	
+
+		
+
+		it('should return an uid sender set via its constructor', function(){
+			var message = new Message({
+				recipient: 'uid---------d'
+			});
+
+			assert.equal(JSON.stringify(message.recipient), '{"uid":"uid---------d"}');
+		});	
+
+		it('should return an object sender set via its constructor', function(){
+			var message = new Message({
+				recipient: {
+				 	  id: 'broker'
+					, applicationId: 'tradr'
+					, version: '0.1.0'
+				}
+			});
+
+			assert.equal(JSON.stringify(message.recipient), '{"id":"broker","applicationId":"tradr","version":"0.1.0"}');
+		});	
+
+
+
+		
+
+		it('should accepts an uid sender set via its constructor as valid', function(){
+			var message = new Message({
+				sender: 'uid---------d'
+			});
+
+			assert(message.isValid);
+		});	
+
+		it('should accepts an object sender set via its constructor as valid', function(){
+			var message = new Message({
+				sender: {
+				 	  id: 'broker'
+					, applicationId: 'tradr'
+					, version: '0.1.0'
+				}
+			});
+
+			assert(message.isValid);
+		});	
+
+		
+
+		it('should accepts an uid recipient set via its constructor as valid', function(){
+			var message = new Message({
+				recipient: 'uid---------d'
+			});
+
+			assert(message.isValid);
+		});	
+
+		it('should accepts an object recipient set via its constructor as valid', function(){
+			var message = new Message({
+				recipient: {
+				 	  id: 'broker'
+					, applicationId: 'tradr'
+					, version: '0.1.0'
+				}
+			});
+
+			assert(message.isValid);
+		});	
 	});
 	
